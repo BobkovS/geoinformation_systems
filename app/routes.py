e2fa436303c205a004cb386b7c9dcc868eaf8494
bobkovs <bobkov@greendatasoft.ru>
2020-02-05 17:46:40 +0500

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+import osmnx as ox
+from flask import request, jsonify, make_response
+
+from app import app
+
+
+@app.route('/town_buildings_variances', methods=["POST"])
+def building_variances():
+    resp_data = {"building_variances": None,
+                 "errors": None}
+    req_json = request.get_json()
+    town_name = req_json["town_name"]
+    try:
+        buildings = ox.footprints_from_place(town_name)
+        resp_data["building_variances"] = buildings['building'].value_counts().to_dict()
+        return make_response(jsonify(resp_data))
+    except Exception:
+        resp_data["errors"] = "Не получается найти выбранное место, попробуйте другое"
+        return make_response(jsonify(resp_data))
